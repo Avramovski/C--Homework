8228965843e6d1a809a247d4a01a86e5ea89e1ff
Avramovski <avramovski04@gmail.com>
2024-03-17 16:18:39 +0100

    nedovrseno

---
+++ Class.Homework/task01/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using task01.myClasses;
+
+Console.WriteLine("Hello, World!");
+
+
+Driver driver1 = new Driver("Danilo", 5);
+Driver driver2 = new Driver("Nikola", 4);
+Driver driver3 = new Driver("Verstappen", 2);
+Driver driver4 = new Driver("Hamilton", 2);
+
+
+Car car1FirstTask = new Car("BMW", 200,driver1);
+Car car2FirstTask = new Car("Nissan", 200,driver2);
+Car car1 = new Car("Ford", 120);
+Car car2 = new Car("Audi", 160);
+Car car3 = new Car("Formula Red Bull", 312);    
+Car car4 = new Car("Formula Mercedes",310);
+
+
+static void RaceCars(Car car1FirstTask,Car car2FirstTask)
+{
+    int raccingCarSpeed1 = car1FirstTask.CalculateSpeed();
+    int raccingCarSpeed2 = car2FirstTask.CalculateSpeed();
+
+    if (raccingCarSpeed1 > raccingCarSpeed2)
+    {
+        Console.WriteLine("Car number1 was fasster");
+    }
+    else if (raccingCarSpeed2 > raccingCarSpeed1)
+    {
+        Console.WriteLine("Car number2 was fasster");
+    }
+    else
+    {
+
+        Console.WriteLine("No winner");
+    }
+}
+
+RaceCars(car1FirstTask, car2FirstTask);
+
+Console.WriteLine("Enter first car:");
+string firstCarInput  = Console.ReadLine();
+
+Console.WriteLine("Enter driver for the firdt car:");
+string driverForFirstCarInput = Console.ReadLine();
+
+Console.WriteLine("Enter second car:");
+string secondCarInput = Console.ReadLine();
+
+
+Console.WriteLine("Enter driver for the second car:");
+string driverForSecondCarInput = Console.ReadLine();
+
+
+
+
---
+++ Class.Homework/task01/myClasses/Car.cs
@@ -0,0 +1,44 @@
+﻿
+namespace task01.myClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Driver Driver { get; set; }
+
+        public Car(string model, int speed, Driver driver)
+        {
+            Model = model;
+            Speed = speed;
+            Driver = driver;
+
+        }
+        public Car(string model, int speed)
+        {
+            Model = model;
+            Speed = speed;
+            
+        }
+
+
+        public int CalculateSpeed()
+        {
+            if (Driver == null)
+            {
+                Console.WriteLine("Driver is not assigned to the car");
+                return 0;
+            }
+            int driverSkill = Driver.Skill;
+            int result = driverSkill * Speed;
+            return result;
+
+        }
+        public int FasterInRace(Car model,Driver driver)
+        {
+            int speedCar1 = CalculateSpeed();
+
+            return speedCar1;
+        }
+    }
+}
